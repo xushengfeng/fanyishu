49f69c4f250c7fd12e3f4c1e2151b683b698690a
xushengfeng <xushengfeng_zg@163.com>
2022-10-14 00:39:36 +0800

    翻译器 将翻译值传递到下一个翻译器

--- src/translator.ts
+++ src/translator.ts
@@ -45,11 +45,11 @@ document.querySelector("textarea").oninput = () => {
 };
 
 let o = {
-    youdao: { t: "有道", f: youdao },
-    baidu: { t: "百度", f: baidu },
-    deepl: { t: "Deepl", f: deepl },
-    caiyun: { t: "彩云", f: caiyun },
-    bing: { t: "必应", f: bing },
+    youdao: { t: "有道" },
+    baidu: { t: "百度" },
+    deepl: { t: "Deepl" },
+    caiyun: { t: "彩云" },
+    bing: { t: "必应" },
 };
 
 type item_type = { id: string; e: string; from: string; to: string; children?: item_type[] };
@@ -103,8 +103,12 @@ class item extends HTMLElement {
     }
 
     set text(t: string) {
-        let f = o[this.t.querySelector("select").value].f;
-        f(t, "", "");
+        engine(this.t.querySelector("select").value, t, "", "").then((v) => {
+            console.log(v);
+            this.c.querySelectorAll(":scope > e-translator").forEach((el: item) => {
+                el.text = v;
+            });
+        });
     }
 
     set e(t: string) {
@@ -132,108 +136,113 @@ render_tree(tree, document.getElementById("translators"));
 
 translate(text);
 
-function youdao(text: string, from: string, to: string) {
-    fetch(`http://fanyi.youdao.com/translate?&doctype=json&type=AUTO&i=${encodeURIComponent(text)}`, {
-        method: "GET",
-    })
-        .then((v) => v.json())
-        .then((t) => {
-            let l = [];
-            for (let i of t.translateResult) {
-                let t = "";
-                for (let ii of i) {
-                    t += ii.tgt;
-                }
-                l.push(t);
-            }
-            document.getElementById("youdao").innerText = l.join("\n");
-        });
-}
+function engine(e: string, text: string, from: string, to: string) {
+    return new Promise((re: (text: string) => void) => {
+        switch (e) {
+            case "youdao":
+                fetch(`http://fanyi.youdao.com/translate?&doctype=json&type=AUTO&i=${encodeURIComponent(text)}`, {
+                    method: "GET",
+                })
+                    .then((v) => v.json())
+                    .then((t) => {
+                        let l = [];
+                        for (let i of t.translateResult) {
+                            let t = "";
+                            for (let ii of i) {
+                                t += ii.tgt;
+                            }
+                            l.push(t);
+                        }
+                        re(l.join("\n"));
+                    });
+                break;
+            case "baidu":
+                if (!api_id.baidu.appid || !api_id.baidu.key) return;
+                let appid = api_id.baidu.appid;
+                let key = api_id.baidu.key;
+                let salt = new Date().getTime();
+                let str1 = appid + text + salt + key;
+                let sign = MD5(str1);
+                fetch(
+                    `http://api.fanyi.baidu.com/api/trans/vip/translate?q=${encodeURIComponent(
+                        text
+                    )}&from=en&to=zh&appid=${appid}&salt=${salt}&sign=${sign}`
+                )
+                    .then((v) => v.json())
+                    .then((t) => {
+                        let l = t.trans_result.map((v) => v.dst);
+                        document.getElementById("baidu").innerText = l.join("\n");
+                    });
+                break;
+            case "deepl":
+                if (!api_id.deepl.key) return;
+                fetch("https://api-free.deepl.com/v2/translate", {
+                    body: `text=${encodeURIComponent(text)}&target_lang=${to}`,
+                    headers: {
+                        Authorization: `DeepL-Auth-Key ${api_id.deepl.key}`,
+                        "Content-Type": "application/x-www-form-urlencoded",
+                    },
+                    method: "POST",
+                })
+                    .then((v) => v.json())
+                    .then((t) => {
+                        let l = t.translations.map((x) => x.text);
+                        document.getElementById("deepl").innerText = l.join("\n");
+                    });
+                break;
+            case "caiyun":
+                if (!api_id.caiyun.token) return;
+                let url = "http://api.interpreter.caiyunai.com/v1/translator";
+                let token = api_id.caiyun.token;
+                let payload = {
+                    source: text.split("\n"),
+                    trans_type: "auto2zh",
+                    request_id: "demo",
+                    detect: true,
+                };
+                let headers = {
+                    "content-type": "application/json",
+                    "x-authorization": "token " + token,
+                };
+                fetch(url, { method: "POST", body: JSON.stringify(payload), headers })
+                    .then((v) => v.json())
+                    .then((t) => {
+                        console.log(t);
+                        document.getElementById("caiyun").innerText = t.target.join("\n");
+                    });
+                break;
+            case "bing":
+                if (!api_id.bing.key) return;
+                fetch(
+                    `https://api.cognitive.microsofttranslator.com/translate?${new URLSearchParams({
+                        "api-version": "3.0",
+                        from: "en",
+                        to: "cn",
+                    }).toString()}`,
+                    {
+                        method: "POST",
+                        headers: {
+                            "Ocp-Apim-Subscription-Key": api_id.bing.key,
+                            "Content-type": "application/json",
+                            "X-ClientTraceId": crypto.randomUUID(),
+                        },
+                        body: JSON.stringify([
+                            {
+                                text: text,
+                            },
+                        ]),
+                    }
+                )
+                    .then((v) => v.json())
+                    .then((t) => {
+                        document.getElementById("bing").innerText = t[0].translations[0].text;
+                    });
+                break;
 
-function baidu(text: string, from: string, to: string) {
-    if (!api_id.baidu.appid || !api_id.baidu.key) return;
-    let appid = api_id.baidu.appid;
-    let key = api_id.baidu.key;
-    let salt = new Date().getTime();
-    let str1 = appid + text + salt + key;
-    let sign = MD5(str1);
-    fetch(
-        `http://api.fanyi.baidu.com/api/trans/vip/translate?q=${encodeURIComponent(
-            text
-        )}&from=en&to=zh&appid=${appid}&salt=${salt}&sign=${sign}`
-    )
-        .then((v) => v.json())
-        .then((t) => {
-            let l = t.trans_result.map((v) => v.dst);
-            document.getElementById("baidu").innerText = l.join("\n");
-        });
+            default:
+                break;
+        }
+    });
 }
 
 import MD5 from "blueimp-md5";
-
-function deepl(text: string, from: string, to: string) {
-    if (!api_id.deepl.key) return;
-    fetch("https://api-free.deepl.com/v2/translate", {
-        body: `text=${encodeURIComponent(text)}&target_lang=${to}`,
-        headers: {
-            Authorization: `DeepL-Auth-Key ${api_id.deepl.key}`,
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        method: "POST",
-    })
-        .then((v) => v.json())
-        .then((t) => {
-            let l = t.translations.map((x) => x.text);
-            document.getElementById("deepl").innerText = l.join("\n");
-        });
-}
-
-function caiyun(text: string, from: string, to: string) {
-    if (!api_id.caiyun.token) return;
-    let url = "http://api.interpreter.caiyunai.com/v1/translator";
-    let token = api_id.caiyun.token;
-    let payload = {
-        source: text.split("\n"),
-        trans_type: "auto2zh",
-        request_id: "demo",
-        detect: true,
-    };
-    let headers = {
-        "content-type": "application/json",
-        "x-authorization": "token " + token,
-    };
-    fetch(url, { method: "POST", body: JSON.stringify(payload), headers })
-        .then((v) => v.json())
-        .then((t) => {
-            console.log(t);
-            document.getElementById("caiyun").innerText = t.target.join("\n");
-        });
-}
-
-function bing(text: string, from: string, to: string) {
-    if (!api_id.bing.key) return;
-    fetch(
-        `https://api.cognitive.microsofttranslator.com/translate?${new URLSearchParams({
-            "api-version": "3.0",
-            from: "en",
-            to: "cn",
-        }).toString()}`,
-        {
-            method: "POST",
-            headers: {
-                "Ocp-Apim-Subscription-Key": api_id.bing.key,
-                "Content-type": "application/json",
-                "X-ClientTraceId": crypto.randomUUID(),
-            },
-            body: JSON.stringify([
-                {
-                    text: text,
-                },
-            ]),
-        }
-    )
-        .then((v) => v.json())
-        .then((t) => {
-            document.getElementById("bing").innerText = t[0].translations[0].text;
-        });
-}
